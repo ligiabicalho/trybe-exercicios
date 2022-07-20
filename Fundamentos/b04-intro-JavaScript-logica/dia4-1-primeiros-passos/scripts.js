9a2f5f632880657007da281071c12ce7a42129c3
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-20 16:19:29 -0300

    exercicios de fixacao - variaveis & tipos primitivos...

--- Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/scripts.js
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/scripts.js
@@ -1,11 +1,33 @@
+/* Exercício fixação variáveis
 const myName = "Lígia";
 const birthCity = "Belo Horizonte";
 let birthYear = 1989;
 
-console.log(myName)
+console.log(myName);
 
 birthYear = 2030;
-console.log(birthYear)
+console.log(birthYear);
 
 // birthCity = "Rio";
-console.log(birthCity)
+console.log(birthCity); */
+
+
+/* Exercício 1 fixação tipos primitivos, tipagem dinâmica e operações aritméticas
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientAge); */
+
+/* Exercício 2 fixação tipos primitivos, tipagem dinâmica e operações aritméticas */
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2 * (base + height);
+console.log(perimeter);
