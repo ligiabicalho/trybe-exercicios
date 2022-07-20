a1626ad1b76b7e984cac0685d348dcbd0fd0c1ed
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-20 15:54:31 -0300

    dia 4.1 exercicio fixacao variaveis

---
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Lígia";
+const birthCity = "Belo Horizonte";
+let birthYear = 1989;
+
+console.log(myName)
+
+birthYear = 2030;
+console.log(birthYear)
+
+// birthCity = "Rio";
+console.log(birthCity)
