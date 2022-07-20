1df2332d4d5a003ce1ea3cc40cf88000c30fabdb
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-20 18:29:19 -0300

    exercicios de fixacao - switch/case

--- Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/scripts.js
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/scripts.js
@@ -18,16 +18,46 @@ let isEnrolled = true;
 const patientInfo = {
   firstName: 'Ana',
   lastName: 'Santos',
+  const patientEmail = 'ana@email.com';
 };
-const patientEmail = 'ana@email.com';
 
 console.log(typeof patientId);
 console.log(typeof patientAge); */
 
-/* Exercício 2 fixação tipos primitivos, tipagem dinâmica e operações aritméticas */
+/* Exercício 2 fixação tipos primitivos, tipagem dinâmica e operações aritméticas 
 const base = 5;
 const height = 8;
 const area = base * height;
 console.log(area);
 const perimeter = 2 * (base + height);
-console.log(perimeter);
+console.log(perimeter); */
+
+
+/* Switch e Case */
+
+mes = 'Trybe';
+let estacaoDoAno = '?';
+
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        estacaoDoAno = 'Verão';
+        break;
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        estacaoDoAno = 'Outono';
+        break;
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        estacaoDoAno = 'Inverno';
+        break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+        estacaoDoAno = 'Primavera';
+}
+
+console.log(estacaoDoAno); // 'Outono'
