c59b14e951c56b9f9559e4efb2a59b725d55b12b
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-21 11:10:32 -0300

    Exercicios práticos - Item 2 e 3 - maior de dois numeros e maior de três numeros

--- Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
@@ -3,7 +3,7 @@ let b = 7;
 
 //1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
 
-let adicao = a + b;
+/* let adicao = a + b;
 console.log(adicao);
 
 let subtração = a - b;
@@ -16,6 +16,27 @@ let divisao = a / b;
 console.log(divisao);
 
 let modulo = a % b;
-console.log(modulo);
+console.log(modulo); */
+
+
+//2. Faça um programa que retorne o maior de dois números.
+
+/* if(a > b){
+    console.log(a);
+} else{
+    console.log(b);
+ } */
+
+ //3. Faça um programa que retorne o maior de três números.
+
+ const c = 12;
+ if(a > b){
+    console.log(a);
+}else if(b > c){
+    console.log(b);
+ }else{
+    console.log(c);
+ }
+
 
 
