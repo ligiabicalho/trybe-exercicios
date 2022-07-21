566eacda37f1349a797b288182488fc2e031c8da
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-21 11:17:33 -0300

    Exercicios práticos - Item 4 - retornar positivo, negativo, zero

--- Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
@@ -16,27 +16,36 @@ let divisao = a / b;
 console.log(divisao);
 
 let modulo = a % b;
-console.log(modulo); */
+console.log(modulo); 
 
 
 //2. Faça um programa que retorne o maior de dois números.
 
-/* if(a > b){
+if(a > b){
     console.log(a);
 } else{
     console.log(b);
- } */
+ } 
 
- //3. Faça um programa que retorne o maior de três números.
+//3. Faça um programa que retorne o maior de três números.
 
- const c = 12;
+const c = 12;
  if(a > b){
     console.log(a);
 }else if(b > c){
     console.log(b);
  }else{
     console.log(c);
- }
+ } 
 
+//4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
+const d = 1
+if(d > 0){
+    console.log('Positivo');
+}else if(d < 0){
+    console.log('Negativo');
+}else{
+    console.log('Zero');
+}
 
 
