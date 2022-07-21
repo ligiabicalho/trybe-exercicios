cb25ebdab0da995945481ca918d0705521a3e291
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-21 10:41:59 -0300

    Exercicios práticos - Item 1 - 5 programas com operadores aritmeticos

---
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
@@ -0,0 +1,21 @@
+let a = 3;
+let b = 7;
+
+//1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+let adicao = a + b;
+console.log(adicao);
+
+let subtração = a - b;
+console.log(subtração)
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
+
+
