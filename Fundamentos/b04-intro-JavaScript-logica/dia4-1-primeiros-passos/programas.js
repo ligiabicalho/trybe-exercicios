ee66614417ab0389e6b54bb7a9615b992d2b1705
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-21 12:15:37 -0300

    Exercicios práticos - Item 6 - xadrex

--- Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-1-primeiros-passos/programas.js
@@ -49,10 +49,10 @@ if(d > 0){
     console.log('Zero');
 } 
 
-//5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. */
+//5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. 
 const ang1 = 60;
 const ang2 = 50;
-const ang3 = -50;
+const ang3 = -1;
 const triangulo = ang1 + ang2 + ang3;
 if(ang1 && ang2 && ang3 > 0){
     if(triangulo === 180){
@@ -62,4 +62,22 @@ if(ang1 && ang2 && ang3 > 0){
     }
 }else{
     console.log('Erro: ângulo inválido!');
+}
+//6.Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz. */
+
+let pecaXadrez = 'Bispoo';
+switch (pecaXadrez.toLowerCase()) {
+    case 'peão': console.log('Avança uma casa para frente. Pode atacar para diagonal. Não pode recuar.');
+        break;
+    case 'bispo': console.log('Diagonais.');
+        break;
+    case 'cavalo': console.log('Se move em "L". O único que pode saltar sobre outra peça.');
+        break;
+    case 'torre': console.log('Horizontais ou verticais.');
+        break;
+    case 'dama': console.log('Diagonais, horizontais ou verticais.');
+        break;
+    case 'rei': console.log('Uma casa, para qualquer direção');
+        break;
+    default: console.log('Opção inválida');
 }
