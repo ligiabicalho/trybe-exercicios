9b027774d568376492e1768659398ff51a0bbb00
Lígia Bicalho <ligiabicalho@gmail.com>
2022-07-22 22:31:08 -0300

    exercicios dia 4.3 - prática e grupo aula ao vivo

---
+++ Fundamentos/b04-intro-JavaScript-logica/dia4-3-logica-algoritmos/practical-exercises.js
@@ -0,0 +1,59 @@
+//1. O fatorial é representado pelo sinal !:  4! = 4 x 3 x 2 x 1 = 24.
+// Crie um algoritmo que retorne o fatorial de 10.
+
+// OPÇÃO 1 - crescente
+// function factorization (factorial) {
+//   let result = 1;
+//   for (let index = 2; index <= factorial; index += 1) {
+//       result = result * index;    
+//   }
+//   return result;
+// } 
+// console.log(factorization(10)); 
+
+// // OPÇÃO 2 - decrescente
+// function factorization (factorial) {
+//   let result = factorial;
+//   for (let index = factorial -1; index >= 1; index -= 1) {
+//       result = result * index;    
+//   }
+//   return result;
+// } 
+// console.log(factorization(10)); 
+
+// 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+// let word = 'trybe';
+// let reverse = '';
+// for (let index = 0; index < word.length; index++) {
+//     reverse = word[index] + reverse;
+// }
+// console.log(reverse);
+
+
+
+// 3- Considere o array de strings abaixo:
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+
+// function comparacao(item1, item2){
+//   return item1.length - item2.length;
+// }
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let arraySort = array.sort(comparacao);
+// let big = arraySort[arraySort.length -1];
+// let small = arraySort[0];
+// console.log('Maior palavra:', big);
+// console.log('Menor palavra:', small);
+
+//4- Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+
+let numPrimo = [];
+for (let num = 2; num < 50; num+=1) {
+  for(let divisor = 2; divisor < num; divisor+=1){
+    if(num % divisor !== 0){
+      numPrimo.push();
+    }
+  }
+}
+console.log(numPrimo);
