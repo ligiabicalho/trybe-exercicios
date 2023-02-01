f33a0d4ab9fae36820aa648909430579a47d72b5
Lígia Bicalho <ligiabicalho@gmail.com>
2023-02-01 15:16:30 -0300

    metodo delete()

--- backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/app.js
+++ backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const OK = 200;
 const CREATED = 201;
+const NOT_FOUND = 404;
 
 app.use(express.json());
 
@@ -48,12 +49,20 @@ app.put('/teams/:id', (req, res) => {
   const updateTeam = teams.find((team) => team.id === Number(id));
 
   if (!updateTeam) {
-    res.status(404).json({ message: 'Team not found' });
+    res.status(NOT_FOUND).json({ message: 'Team not found' });
   }
 
   updateTeam.name = name;
   updateTeam.initials = initials;
-  res.status(200).json({ updateTeam });
+  res.status(OK).json({ updateTeam });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(OK).end();
 });
 
 module.exports = app;
