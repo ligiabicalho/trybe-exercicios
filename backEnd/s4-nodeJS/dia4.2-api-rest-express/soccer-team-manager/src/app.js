d69924762bea67fbc6668c71ba564170a401d471
Lígia Bicalho <ligiabicalho@gmail.com>
2023-02-01 14:57:21 -0300

    Utilizar o VSCode Thunder Client para fazer requisições a partir do VSCode

--- backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/app.js
+++ backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/app.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'Olá Mundo!' });
+// const OK = 200;
+const CREATED = 201;
+
+app.use(express.json());
+
+// app.get('/', (_req, res) => {
+//   res.status(OK).json({ message: 'Olá Mundo!' });
+// });
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(CREATED).json({ team: newTeam });
 });
 
 module.exports = app;
--- backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/server.js
+++ backEnd/s4-nodeJS/dia4.2-api-rest-express/soccer-team-manager/src/server.js
@@ -1,4 +1,3 @@
-// src/server.js
 const app = require('./app');
 
 app.listen(3001, () => console.log('Servidor online na porta 3001'));
