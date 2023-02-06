024e63b9d632601e5add1e946ce6898f5bbef87a
Lígia Bicalho <ligiabicalho@gmail.com>
2023-02-06 14:48:39 -0300

    feat variaveis de ambiente e arq routes

--- backEnd/s4-nodeJS/dia4.5-nodeJS-express-mySQL/trybecash-api/src/app.js
+++ backEnd/s4-nodeJS/dia4.5-nodeJS-express-mySQL/trybecash-api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
+// retorna um middleware p/ interpretar body no formato json
 
 module.exports = app;
