197180fe2f4701c998c5a16018c54af5185e47e6
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-20 18:54:30 -0300

    POO Heranca exercicio 2

--- backEnd/s9-POO-SOLID/dia9.2-heranca-composicao/src/para-fixar-1.ts
+++ backEnd/s9-POO-SOLID/dia9.2-heranca-composicao/src/para-fixar-1.ts
@@ -1,23 +1,28 @@
-// herança sintaxe geral
+// herança sintaxe geral - para fixar 1
 
 export class SuperClass{
   constructor(public isSuper = true){
   }
 
-  sayHello(){
+  protected sayHello(){
     console.log('Olá Mundo!');
   };
 }
 
 export class SubClass extends SuperClass{
+  public sayHello2(){
+    this.sayHello();
+  };
 }
 
-  function myFunc(obj: SuperClass){
-    obj.sayHello();
+  function myFunc(obj: SubClass){
+    obj.sayHello2();
   };
 
-  const obj1 = new SuperClass();
+  // const obj1 = new SuperClass();
   const obj2 = new SubClass();
 
-  myFunc(obj1);
+  // myFunc(obj1);
   myFunc(obj2);
+
+  // herança sintaxe geral - para fixar 2
