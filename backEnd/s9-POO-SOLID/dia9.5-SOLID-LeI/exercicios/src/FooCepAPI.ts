b439d6527121384d9b1569f207426bf26fc4baa4
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-23 19:20:20 -0300

    SOLID exercicios

---
+++ backEnd/s9-POO-SOLID/dia9.5-SOLID-LeI/exercicios/src/FooCepAPI.ts
@@ -0,0 +1,11 @@
+import ICepAPI from './IFooCepAPI';
+
+export default class FooCepAPI implements ICepAPI {
+  async getAddressByCEP(cep: string, number: number): Promise<string> {
+    return `O endereço para o "CEP:${cep}, n°:${number}" é "endereço foo"`;
+  }
+
+  async getCepByAddress(address: string, number: number): Promise<string> {
+    return `O CEP para: "${address}, n°: ${number}" é "CEP baz"`;
+  }
+}
