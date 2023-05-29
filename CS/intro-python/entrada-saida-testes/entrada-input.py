2c7ec20d908a419618bbe95e68cc7e7305f970b3
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-29 16:34:21 -0300

    exercicios

---
+++ CS/intro-python/entrada-saida-testes/entrada-input.py
@@ -0,0 +1,7 @@
+# coletar dados do terminal através da função input
+# input() -> sempre retorna uma string
+# tem como parâmetro opcional o prompt
+
+
+user_input = input("Digite uma mensagem: ")
+print('O valor recebido foi:', user_input)
