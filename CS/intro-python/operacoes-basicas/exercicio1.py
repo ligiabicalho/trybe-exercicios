f1a90b894083994aeb126dc732ef37d998a39c6b
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-30 15:53:37 -0300

    exercicio operacoes basicas

---
+++ CS/intro-python/operacoes-basicas/exercicio1.py
@@ -0,0 +1,39 @@
+a = 10
+b = 5
+
+
+def sum(a, b):
+    return print(a + b)
+
+
+def sub(a, b):
+    return print(a - b)
+
+
+def mult(a, b):
+    return print(a * b)
+
+
+def div(a, b):
+    return print(a / b)
+
+
+def div_int(a, b):
+    return print(a // b)
+
+
+def rest_div(a, b):
+    return print(a % b)
+
+
+def pot(a, b):
+    return print(a ** b)
+
+
+sum(a, b)
+sub(a, b)
+mult(a, b)
+div(a, b)
+div_int(a, b)
+rest_div(a, b)
+pot(a, b)
